9023fb88354ff0c8a581e7b355da6694cec007cc
Dmitriy <dimryb@bk.ru>
2023-05-31 00:15:49 +0700

    Добавил навигацию на другие фрагменты

--- app/src/main/java/space/rybakov/campus/presentation/HomeFragment.kt
+++ app/src/main/java/space/rybakov/campus/presentation/HomeFragment.kt
@@ -1,14 +1,18 @@
 package space.rybakov.campus.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import space.rybakov.campus.R
 import space.rybakov.campus.databinding.FragmentHomeBinding
+import kotlin.math.log
 
 class HomeFragment : Fragment() {
 
@@ -27,6 +31,7 @@ class HomeFragment : Fragment() {
             false
         )
         createGroupsSpinner()
+        setupListeners()
         return binding.root
     }
 
@@ -35,6 +40,26 @@ class HomeFragment : Fragment() {
         ArrayAdapter(requireContext(), R.layout.spinner_list, groupNames).also { adapter ->
             val spinner: Spinner = binding.panelTop.groupsSpinner
             spinner.adapter = adapter
+        }
+    }
+
+    private fun setupListeners() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            Log.d("", item.toString())
+            when (item.itemId) {
+                R.id.home_button -> {
+                }
+                R.id.ads_button -> {
+                    findNavController().navigate(R.id.adsFragment)
+                }
+                R.id.reviews_button -> {
+                    findNavController().navigate(R.id.reviewsFragment)
+                }
+                R.id.settings_button -> {
+                    findNavController().navigate(R.id.settingsFragment)
+                }
+            }
+            true
         }
     }
 
