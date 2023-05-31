b03476fc648f58d6d1cd4280a1840cbb8548d3e2
Dmitriy <dimryb@bk.ru>
2023-05-31 22:24:21 +0700

    Перенес меню в основное активати

--- app/src/main/java/space/rybakov/campus/presentation/HomeFragment.kt
+++ app/src/main/java/space/rybakov/campus/presentation/HomeFragment.kt
@@ -1,18 +1,14 @@
 package space.rybakov.campus.presentation
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import androidx.core.view.get
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import space.rybakov.campus.R
 import space.rybakov.campus.databinding.FragmentHomeBinding
-import kotlin.math.log
 
 class HomeFragment : Fragment() {
 
@@ -31,7 +27,6 @@ class HomeFragment : Fragment() {
             false
         )
         createGroupsSpinner()
-        setupListeners()
         return binding.root
     }
 
@@ -40,26 +35,6 @@ class HomeFragment : Fragment() {
         ArrayAdapter(requireContext(), R.layout.spinner_list, groupNames).also { adapter ->
             val spinner: Spinner = binding.panelTop.groupsSpinner
             spinner.adapter = adapter
-        }
-    }
-
-    private fun setupListeners() {
-        binding.bottomNavigation.setOnItemSelectedListener { item ->
-            Log.d("", item.toString())
-            when (item.itemId) {
-                R.id.home_button -> {
-                }
-                R.id.ads_button -> {
-                    findNavController().navigate(R.id.adsFragment)
-                }
-                R.id.reviews_button -> {
-                    findNavController().navigate(R.id.reviewsFragment)
-                }
-                R.id.settings_button -> {
-                    findNavController().navigate(R.id.settingsFragment)
-                }
-            }
-            true
         }
     }
 
