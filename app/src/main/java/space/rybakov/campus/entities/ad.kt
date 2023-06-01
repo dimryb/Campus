d72fdc65940246fd1fc9e75e5d6bba6ee16a82ef
Dmitriy <dimryb@bk.ru>
2023-06-01 22:52:17 +0700

    Получение в фрагмент оъявлений

--- app/src/main/java/space/rybakov/campus/entities/ad.kt
+++ app/src/main/java/space/rybakov/campus/entities/ad.kt
@@ -2,6 +2,6 @@ package space.rybakov.campus.entities
 
 data class Ad(
     val id: Int,
-    val title: String = "",
+    val title: String = "Заголовок",
     val content: String,
 )
--- app/src/main/java/space/rybakov/campus/presentation/MainViewModel.kt
+++ app/src/main/java/space/rybakov/campus/presentation/MainViewModel.kt
@@ -3,8 +3,10 @@ package space.rybakov.campus.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import space.rybakov.campus.domain.Repository
 import space.rybakov.campus.entities.Ad
 import javax.inject.Inject
@@ -13,5 +15,16 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val repository: Repository,
 ) : ViewModel() {
-    val data: LiveData<List<Ad>> = repository.ads.asLiveData(Dispatchers.Default)
+    val ads: LiveData<List<Ad>> = repository.ads.asLiveData(Dispatchers.Default)
+
+    fun getAds(){
+        viewModelScope.launch{
+            try {
+                repository.getAds()
+            } catch (e: Exception){
+                TODO("нужно сделать")
+            }
+        }
+    }
+
 }
